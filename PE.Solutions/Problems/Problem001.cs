9d58837ef19b7435c035922017b5f94fcd1a1909
thanhdoan <thanh.doan1@rbi.co.uk>
2019-02-28 21:06:27 +0000

    Thanh | Optimize Problem001.cs

--- PE.Solutions/Problems/Problem001.cs
+++ PE.Solutions/Problems/Problem001.cs
@@ -6,14 +6,70 @@ namespace PE.Solutions.Problems
         public long SumMultipleThreeAndFive(long n)
         {
             long sum = 0;
-            for (int i = 0; i < n; i++)
+            for (long i = 0; i < n; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+
+        public long SumMultipleThreeAndFiveTwo(long number)
+        {
+            var sum1 = SumMultipleThree(number);
+            var sum2 = SumMultipleFive(number);
+            var sum3 = SumMultipleFifteen(number);
+            return SumMultipleThree(number) + SumMultipleFive(number) - SumMultipleFifteen(number);
+        }
+
+        private long SumMultipleFifteen(long n)
+        {
+            long sum = 0;
+            for (long i = 0; i < n; i++)
+            {
+                if (i % 15 == 0)
                 {
                     sum += i;
                 }
             }
             return sum;
+        }
+
+        private long SumMultipleFive(long n)
+        {
+            long sum = 0;
+            for (long i = 0; i < n; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        private long SumMultipleThree(long n)
+        {
+            long sum = 0;
+            for (long i = 0; i < n; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        private long SumDivisibleBy(long target, long number)
+        {
+            long p = (target / number);
+            return number * (p * (p + 1)) / 2;
+        }
+        public long SumDivisibleBy(long target)
+        {
+            return SumDivisibleBy(target - 1, 3) + SumDivisibleBy(target - 1, 5) - SumDivisibleBy(target - 1, 15);
         }
     }
 }
--- PE.Tests/Problems/Problem001Test.cs
+++ PE.Tests/Problems/Problem001Test.cs
@@ -14,11 +14,29 @@ namespace PE.Tests.Problems
 
         [Theory]
         [InlineData(10L, 23L)]
+        [InlineData(15L, 45L)]
         [InlineData(1000L, 233168L)]
-        [InlineData(1000000000L, 233168L)]
         public void ShouldReturnCorrectSumOfMultipleThreeAndFive(long number, long answer)
         {
             var result = problemOne.SumMultipleThreeAndFive(number);
+
+            result.Should().Be(answer);
+        }
+
+        [Theory]
+        [InlineData(15L, 45L)]
+        public void ShouldReturnCorrectSumWithNewImplementation(long number, long answer)
+        {
+            var result = problemOne.SumMultipleThreeAndFiveTwo(number);
+
+            result.Should().Be(answer);
+        }
+
+        [Theory]
+        [InlineData(15L, 45L)]
+        public void ShouldReturnCorrectSumWithTarget(long number, long answer)
+        {
+            var result = problemOne.SumDivisibleBy(number);
 
             result.Should().Be(answer);
         }
